312885bb51ac1f154992a1f23142834c79288327
Michael Zhang <failed.down@gmail.com>
2017-07-12 21:11:48 -0500

    Read world from file.

--- src/level.gd
+++ src/level.gd
@@ -1,20 +1,50 @@
 const DELIMITER = "---"
 
 class Level:
-	var _tilemap
-	func _init(tilemap):
-		self._tilemap = tilemap
+	var size
+	var tilemap
+	func _init(size, tilemap):
+		self.size = size
+		self.tilemap = tilemap
+	func get_tile_at(x, y):
+		return self.tilemap[x * self.size.y + y]
+	func initialize(node):
+		for x in range(self.size.x):
+			for y in range(self.size.y):
+				var tile = -1
+				var c = self.get_tile_at(x, y)
+				if c == "#":
+					tile = 0
+				node.set_cell(y, x, tile)
 
-	static func load_from_file(filename):
-		var fullpath = "res://levels/" + filename
-		var file = File.new()
-		if not file.file_exists(fullpath):
-			print("You done fucked.")
-			return
+func load_from_file(filename):
+	var fullpath = "res://levels/" + filename
+	var file = File.new()
+	if not file.file_exists(fullpath):
+		print("You done fucked.")
+		return
 
-		file.open(fullpath, file.READ)
-		var content = file.get_as_text()
-		file.close()
+	file.open(fullpath, file.READ)
+	var content = file.get_as_text()
+	file.close()
 
-		var parts = content.split("\n", true)
-		print(parts)
+	var parts = content.split("\n", true)
+	
+	var lc = 0
+	var level_size = Vector2(0, 0)
+	var level_array = Array()
+	for line in parts:
+		if line == "---":
+			break
+		lc += 1
+		level_size.x = max(level_size.x, lc)
+		level_size.y = max(level_size.y, line.length())
+		level_array.append(line)
+	
+	var tiles = Array()
+	for line in level_array:
+		for c in line:
+			tiles.append(c)
+	
+	var level = Level.new(level_size, tiles)
+	return level
--- src/world.gd
+++ src/world.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-const Level = preload("res://src/level.gd").Level
+const Level = preload("res://src/level.gd")
 
 func _ready():
 	var level = Level.load_from_file("0.txt")
 	var tilemap = get_node("tilemap")
+	
+	# fill tilemap with level contents
+	level.initialize(tilemap)
